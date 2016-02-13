8ba2a5249df23c0002b005c39d73214ffef56dbd
mickythump <mikolaj06120@gmail.com>
2016-02-13 15:51:38 +0100

    todo app

---
+++ App.jsx
@@ -0,0 +1,110 @@
+injectTapEventPlugin();
+
+var {
+  AppCanvas,
+  AppBar,
+  Styles,
+  RaisedButton,
+  DatePicker,
+  TextField
+} = MUI;
+
+var { ThemeManager, LightRawTheme } = Styles;
+
+App = React.createClass({
+  mixins: [ReactMeteorData],
+
+  childContextTypes: {
+        muiTheme: React.PropTypes.object
+    },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
+    };
+  },
+
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
+
+  getMeteorData() {
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      query = { checked: { $ne: true } };
+    }
+
+    return {
+      tasks: Tasks.find(query, { sort: { createdAt: -1 }}).fetch(),
+      incompleteCount: Tasks.find({ checked: { $ne: true }}).count(),
+      currentUser: Meteor.user()
+    }
+  },
+
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      const currentUserId = this.data.currentUser && this.data.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return <Task
+        key={task._id}
+        task={task}
+        showPrivateButton={showPrivateButton} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+    Meteor.call("addTask", text);
+
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  },
+
+  render() {
+    return(
+      //<AppCanvas>
+        <div className="container">
+          <header>
+            <h1>Todo List ({this.data.incompleteCount})</h1>
+          </header>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
+
+          <AccountsUIWrapper />
+
+          {this.data.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit} >
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks" />
+            </form> : ''
+          }
+
+          <ul>
+            {this.renderTasks()}
+          </ul>
+        </div>
+      //</AppCanvas>
+    );
+  }
+});
